dc5532042790efd5ea3376d1b794746d7e6d2275
Arthur Simões Ferreira <asferreira@id.uff.br>
2024-06-28 00:25:01 -0300

    fix: removed line error

--- src/server.js
+++ src/server.js
@@ -37,5 +37,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    GameController.intializeBoard();
 });
